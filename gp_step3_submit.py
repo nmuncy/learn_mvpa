095d9fd4c1c61f8ae289411b69eea9dd8469a405
Nathan Muncy <nmuncy@fiu.edu>
2020-12-03 11:44:02 -0500

    working, need to restructure for updates

--- gp_step3_submit.py
+++ gp_step3_submit.py
@@ -72,31 +72,33 @@ def main():
     subj_list = [x for x in os.listdir(deriv_dir) if fnmatch.fnmatch(x, "sub-*")]
     subj_list.sort()
 
-    # for i in subj_list:
-    i = subj_list[0]
-    for j in sess_list:
+    for i in subj_list:
+        # i = subj_list[0]
+        for j in sess_list:
 
-        h_out = os.path.join(out_dir, f"out_{i}_{j}.txt")
-        h_err = os.path.join(out_dir, f"err_{i}_{j}.txt")
+            h_out = os.path.join(out_dir, f"out_{i}_{j}.txt")
+            h_err = os.path.join(out_dir, f"err_{i}_{j}.txt")
 
-        # write decon_dict to json in subj dir
-        with open(os.path.join(deriv_dir, i, j, "decon_dict.json"), "w") as outfile:
-            json.dump(decon_dict, outfile)
+            # write decon_dict to json in subj dir
+            with open(os.path.join(deriv_dir, i, j, "decon_dict.json"), "w") as outfile:
+                json.dump(decon_dict, outfile)
 
-        sbatch_job = f"""
-            sbatch \
-            -J "GP3{i.split("-")[1]}" -t 10:00:00 --mem=4000 --ntasks-per-node=1 \
-            -p centos7_IB_44C_512G  -o {h_out} -e {h_err} \
-            --account iacc_madlab --qos pq_madlab \
-            --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
-            python {code_dir}/gp_step3_decon.py {i} {j} {decon_type} {deriv_dir}"
-        """
-        # {' '.join(decon_dict.keys())}
+            sbatch_job = f"""
+                sbatch \
+                -J "GP3{i.split("-")[1]}" -t 30:00:00 --mem=4000 --ntasks-per-node=1 \
+                -p centos7_IB_44C_512G  -o {h_out} -e {h_err} \
+                --account iacc_madlab --qos pq_madlab \
+                --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
+                python {code_dir}/gp_step3_decon.py {i} {j} {decon_type} {deriv_dir}"
+            """
+            # {' '.join(decon_dict.keys())}
 
-        sbatch_submit = subprocess.Popen(sbatch_job, shell=True, stdout=subprocess.PIPE)
-        job_id = sbatch_submit.communicate()[0]
-        print(job_id)
-        time.sleep(5)
+            sbatch_submit = subprocess.Popen(
+                sbatch_job, shell=True, stdout=subprocess.PIPE
+            )
+            job_id = sbatch_submit.communicate()[0]
+            print(job_id)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
