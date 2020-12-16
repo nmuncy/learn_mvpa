f897d62908f255f3da47f2f79b2855b03645157e
Nathan Muncy <nmuncy@fiu.edu>
2020-12-16 16:32:42 -0500

    some progress, hyper breaks

--- mvpa_step3_test.py
+++ mvpa_step3_test.py
@@ -28,36 +28,43 @@ if not os.path.exists(group_path):
 
 # %%
 """
-Step 1: Train Between, Within Classifiers
-
-TODO:
-    Figure out how to embed stats, since we are missing
-        "errorfx=mean_match_accuracy" from cross-validation.
-        Approach: set_postproc to fsclf
+Step 1: Import Training Data
 """
 # get data
 fds_train = h5load(os.path.join(hdf5_path, "model1_data_Train.hdf5.gz"))
 
+# embed subj num
 for i, sd in enumerate(fds_train):
     sd.sa["subject"] = np.repeat(i, len(sd))
 train_nsubj = len(fds_train)
 train_ncats = len(fds_train[0].UT)
 train_nruns = len(fds_train[0].UC)
 
-# # write out summary
-# h_out = """
-#     Number of Subjects: {}
-#     Number of Categories: {}
-#     Number of Runs: {}
+# write out dataset summary
+h_out = """
+    Number of Subjects: {}
+    Number of Categories: {}
+    Number of Runs: {}
 
-#     {}
-# """.format(
-#     train_nsubj, train_ncats, train_nruns, fds_train[0].summary()
-# )
-# write_out = open(os.path.join(group_path, "mvpa_train_summary.txt"), "w")
-# write_out.write(h_out)
-# write_out.close()
+    {}
+""".format(
+    train_nsubj, train_ncats, train_nruns, fds_train[0].summary()
+)
+write_out = open(os.path.join(group_path, "mvpa_train_summary.txt"), "w")
+write_out.write(h_out)
+write_out.close()
 
+
+# %%
+"""
+Step 2: Train Classifiers
+
+TODO:
+    Figure out how to embed stats, since we are missing
+        "errorfx=mean_match_accuracy" from cross-validation.
+        Approach: set_postproc to fsclf
+    Update: working?
+"""
 # Set classifier model
 clf = LinearCSVMC()
 
@@ -76,30 +83,18 @@ fsclf = FeatureSelectionClassifier(clf, sbfs)
 fsclf.set_postproc(BinaryFxNode(mean_match_accuracy, 'targets'))
 
 # Train classifier on all data
-#   don't cross validate
+#   don't cross-validate
 fds_train_all = vstack(fds_train)
 fsclf.train(fds_train_all)
 print(fsclf.summary())
 
 # garbage collect
-del fds_train
-# del fds_train_all
-
-
-# # Classify within, between
-# # within
-# cvws = CrossValidation(
-#     fsclf, NFoldPartitioner(attr="chunks"), errorfx=mean_match_accuracy
-# )
-# # between
-# cvbs = CrossValidation(
-#     fsclf, NFoldPartitioner(attr="subject"), errorfx=mean_match_accuracy
-# )
+del fds_train_all
 
 
 # %%
 """
-Step 2: Test classifiers
+Step 3: Import Test Data
 """
 # get test data
 fds_test = h5load(os.path.join(hdf5_path, "model1_data_Test.hdf5.gz"))
@@ -110,30 +105,37 @@ test_nsubj = len(fds_test)
 test_ncats = len(fds_test[0].UT)
 test_nruns = len(fds_test[0].UC)
 
-# # write out summary
-# h_out = """
-#     Number of Subjects: {}
-#     Number of Categories: {}
-#     Number of Runs: {}
+h_out = """
+    Number of Subjects: {}
+    Number of Categories: {}
+    Number of Runs: {}
 
-#     {}
-# """.format(
-#     test_nsubj, test_ncats, test_nruns, fds_test[0].summary()
-# )
-# write_out = open(os.path.join(group_path, "mvpa_test_summary.txt"), "w")
-# write_out.write(h_out)
-# write_out.close()
+    {}
+""".format(
+    test_nsubj, test_ncats, test_nruns, fds_test[0].summary()
+)
+write_out = open(os.path.join(group_path, "mvpa_test_summary.txt"), "w")
+write_out.write(h_out)
+write_out.close()
 
 
+# %%
+"""
+Step 4: Test Classifiers on test data
+    Need both within, between subject tests
+
+    Update - dropped different cross-validators that
+        had embedded bs/ws parcellating
+
+    TODO: verify that I'm using bs/ws correctly
+"""
 # Within-subject - test classifier on e/subject targets
-# wsc_results = [cvws(sd) for sd in fds_test]
 wsc_results = [fsclf(x) for x in fds_test]
 wsc_results = vstack(wsc_results)
 
 # Between-subject - test classifier on all targets
 #   (collapse subject)
 fds_comb = vstack(fds_test)
-# bsc_results = cvbs(fds_comb)
 bsc_results = fsclf(fds_comb)
 
 # garbage collect
@@ -141,11 +143,13 @@ del fds_comb
 
 
 # %%
+"""
+Step 5: Make some output sans hyperalignment
+"""
 # features
 anova = OneWayAnova()
 fscores = [anova(sd) for sd in fds_test]
 fscores = np.mean(np.asarray(vstack(fscores)), axis=0)
-
 ds_fs = [sd[:, fselector(fscores)] for sd in fds_test]
 
 # within
@@ -162,7 +166,6 @@ sm_anat = np.corrcoef(ds_fs.get_mapped(mean_group_sample(["targets"])))
 intended_label_order = [0, 1]
 labels = fds_test[0].UT
 labels = labels[intended_label_order]
-
 pl.figure(figsize=(12, 12))
 for i, sm_t in enumerate(
     (
@@ -184,9 +187,9 @@ for i, sm_t in enumerate(
         range(test_ncats), labels, size="small", stretch="ultra-condensed", rotation=45
     )
     pl.ylim(ylim)
+pl.savefig(os.path.join(group_path, "mvpa_plot_classifiers.png"))
 
-# pl.savefig(os.path.join(group_path, "mvpa_plot_classifiers.png"))
-
+# text file
 h_out = """
     Classifier = LinearSVM
 
@@ -207,41 +210,44 @@ write_out.close()
 
 # %%
 """
-Step 3: Train, Test Hyperalignment
+Step 6: Train, Test Hyperalignment
+
+TODO: hypmaps breaking from differences in samples (388, 389),
+        fscores_hyper gives error "divide by zero encountered
+        in divide f = msb / msw"
+        So, something could be wrong in training data
 """
-# classifier
-cv = CrossValidation(clf, NFoldPartitioner(
-    attr="subject"), errorfx=mean_match_accuracy)
+# # cross-validation
+# cv = CrossValidation(clf, NFoldPartitioner(
+#     attr="subject"), errorfx=mean_match_accuracy)
 
 bsc_hyper_results = []
-for test_run in range(nruns):
 
-    ds_train = [sd[sd.sa.chunks != test_run, :] for sd in fds_train]
-    ds_test = [sd[sd.sa.chunks == test_run, :] for sd in fds_train]
+# Train
+anova = OneWayAnova()
+fscores_hyper = [anova(sd) for sd in fds_train]
+featsels = [StaticFeatureSelection(
+    fselector(fscore)) for fscore in fscores_hyper]
+fds_train_fs = [fs.forward(sd) for fs, sd in zip(featsels, fds_train)]
 
-    anova = OneWayAnova()
-    fscores = [anova(sd) for sd in ds_train]
-    featsels = [StaticFeatureSelection(fselector(fscore))
-                for fscore in fscores]
-    ds_train_fs = [fs.forward(sd) for fs, sd in zip(featsels, ds_train)]
+hyper = Hyperalignment()
+hypmaps = hyper(fds_train_fs)
 
-    hyper = Hyperalignment()
-    hypmaps = hyper(ds_train_fs)
 
-    ds_test_fs = [fs.forward(sd) for fs, sd in zip(featsels, ds_test)]
-    ds_hyper = [h.forward(sd) for h, sd in zip(hypmaps, ds_test_fs)]
+# ds_test_fs = [fs.forward(sd) for fs, sd in zip(featsels, ds_test)]
+# ds_hyper = [h.forward(sd) for h, sd in zip(hypmaps, ds_test_fs)]
 
-    ds_hyper = vstack(ds_hyper)
-    zscore(ds_hyper, chunks_attr="subject")
-    res_cv = cv(ds_hyper)
-    bsc_hyper_results.append(res_cv)
+# ds_hyper = vstack(ds_hyper)
+# zscore(ds_hyper, chunks_attr="subject")
+# res_cv = cv(ds_hyper)
+# bsc_hyper_results.append(res_cv)
 
-bsc_hyper_results = hstack(bsc_hyper_results)
+# bsc_hyper_results = hstack(bsc_hyper_results)
 
 
 # %%
 """
-Step 4: Compare Classifiers
+Step 7: Compare Classifiers
 """
 h_out = """
     Classifier = LinearSVM
@@ -268,7 +274,7 @@ write_out.close()
 
 # %%
 """
-Step 5: Show Similarity
+Step 8: Show Similarity
 """
 anova = OneWayAnova()
 fscores = [anova(sd) for sd in fds_train]
