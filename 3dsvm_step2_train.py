9d1e6c721082bbd45d7e7cde2ec869d07ff19ca5
Nathan Muncy <nmuncy@fiu.edu>
2021-01-07 16:12:46 -0500

    train, test working?

--- 3dsvm_step2_train.py
+++ 3dsvm_step2_train.py
@@ -4,6 +4,7 @@ Notes
 
 # %%
 import os
+import subprocess
 from gp_step0_dcm2nii import func_sbatch
 
 # %%
@@ -17,27 +18,33 @@ subj_dir = os.path.join(work_dir, subj, sess)
 mask = os.path.join(work_dir, "grpAnalysis/Group_Int_Mask.nii.gz")
 
 # %%
-if not os.path.exists(os.path.join(subj_dir, "3dSVM_train+tlrc.HEAD")):
-    h_cmd = f"""
-        cd {subj_dir}
-        3dsvm -trainvol 3dSVM_{train}+tlrc \
-            -trainlabels 3dSVM_{train}_categories.txt \
-            -mask {mask} \
-            -model 3dSVM_train
-    """
-    func_sbatch(h_cmd, 1, 1, 1, f"{subj.split('-')[1]}trn", subj_dir)
+h_cmd = f"""
+    module load afni-20.2.06
+    cd {subj_dir}
+    3dsvm -trainvol 3dSVM_{train}+tlrc \
+        -trainlabels 3dSVM_{train}_categories.txt \
+        -mask {mask} \
+        -model 3dSVM_train
+"""
+h_train = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+train_out = h_train.communicate()[0].decode("utf-8")
+# print(out)
+f = open(f"{subj_dir}/3dSVM_train_out.txt", "w")
+f.write(train_out)
+f.close()
 
 
 # %%
-if not os.path.exists(os.path.join(subj_dir, "3dSVM_test_overall_DAG.1D")):
-    h_cmd = f"""
-        cd {subj_dir}
-        3dsvm -testvol 3dSVM_{test}+tlrc \
-            -model 3dSVM_train+tlrc \
-            -testlabels 3dSVM_{test}_categories.txt \
-            -predictions 3dSVM_test
-    """
-    func_sbatch(h_cmd, 1, 1, 1, f"{subj.split('-')[1]}tst", subj_dir)
-
+h_cmd = f"""
+    module load afni-20.2.06
+    cd {subj_dir}
+    3dsvm -testvol 3dSVM_{test}+tlrc \
+        -model 3dSVM_train+tlrc \
+        -testlabels 3dSVM_{test}_categories.txt \
+        -predictions 3dSVM_pred > 3dSVM_pred.txt 2>&1
+"""
+# func_sbatch(h_cmd, 1, 1, 1, f"{subj.split('-')[1]}tst", subj_dir)
+h_test = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+test_out = h_test.communicate()
 
 # %%
