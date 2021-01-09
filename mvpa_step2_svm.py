fe8a86b1e68e6e33d657fba0ac1b49bcdc778fac
Nathan Muncy <nmuncy@fiu.edu>
2021-01-08 20:14:59 -0500

    full train works

--- mvpa_step2_svm.py
+++ mvpa_step2_svm.py
@@ -62,13 +62,7 @@ if not os.path.exists(os.path.join(group_dir, "3dSVM_train+tlrc.HEAD")):
             -model 3dSVM_train \
             > 3dSVM_train_acc.txt 2>&1
     """
-    func_sbatch(h_cmd, 1, 4, 4, "MVPAtrn", group_dir)
-
-    # h_train = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
-    # train_out = h_train.communicate()[0].decode("utf-8")
-    # # f = open(f"{subj_dir}/3dSVM_train_out.txt", "w")
-    # f.write(train_out)
-    # f.close()
+    func_sbatch(h_cmd, 4, 6, 4, "MVPAtrn", group_dir)
 
 
 # %%
@@ -76,44 +70,45 @@ subj = "sub-005"
 subj_dir = os.path.join(work_dir, subj, sess)
 
 
-# for test in test_list:
-test = "Study_BE"
+for test in test_list:
+    # test = "Study_BE"
 
-# Get only test sub-bricks
-#   doesn't affect output, but I like it
-if not os.path.exists(os.path.join(subj_dir, f"3dSVM_{test}_test+tlrc.HEAD")):
-    df = pd.read_csv(
-        os.path.join(subj_dir, f"3dSVM_{test}_categories.txt"), sep=" ", header=None
-    )
-    df.columns = ["cat"]
+    # Get only test sub-bricks
+    #   doesn't affect output, but I like it
+    if not os.path.exists(os.path.join(subj_dir, f"3dSVM_{test}_test+tlrc.HEAD")):
+        df = pd.read_csv(
+            os.path.join(subj_dir, f"3dSVM_{test}_categories.txt"), sep=" ", header=None
+        )
+        df.columns = ["cat"]
 
-    df_new = df[df["cat"] != 9999]
-    df_out = os.path.join(subj_dir, f"3dSVM_{test}_cat_updated.txt")
-    np.savetxt(df_out, df_new["cat"].values, fmt="%s", delimiter=" ")
+        df_new = df[df["cat"] != 9999]
+        df_out = os.path.join(subj_dir, f"3dSVM_{test}_cat_updated.txt")
+        np.savetxt(df_out, df_new["cat"].values, fmt="%s", delimiter=" ")
 
-    df_list = df_new.index.values.tolist()
-    h_cmd = f"""
-        module load afni-20.2.06
-        cd {subj_dir}
-        3dTcat -prefix 3dSVM_{test}_test 3dSVM_{test}+tlrc[{",".join(str(i) for i in df_list)}]
-    """
-    h_cat = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
-    h_cat.communicate()
+        df_list = df_new.index.values.tolist()
+        h_cmd = f"""
+            module load afni-20.2.06
+            cd {subj_dir}
+            3dTcat -prefix 3dSVM_{test}_test 3dSVM_{test}+tlrc[{",".join(str(i) for i in df_list)}]
+        """
+        h_cat = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+        h_cat.communicate()
 
-# Test
-if not os.path.exists(os.path.join(subj_dir, f"3dSVM_pred_{test}_acc.txt")):
-    h_cmd = f"""
-        module load afni-20.2.06
-        cd {subj_dir}
-        3dsvm -testvol 3dSVM_{test}_test+tlrc \
-            -model {group_dir}/3dSVM_train+tlrc \
-            -testlabels 3dSVM_{test}_cat_updated.txt \
-            -predictions 3dSVM_pred_{test} \
-            -classout \
-            2> 3dSVM_pred_{test}_acc.txt
-    """
-    h_test = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
-    test_out = h_test.communicate()
-    print(test, test_out)
+    # Test
+    if not os.path.exists(os.path.join(subj_dir, f"3dSVM_pred_{test}_acc.txt")):
+        h_cmd = f"""
+            # module load afni-20.2.06
+            cd {subj_dir}
+            3dsvm -testvol 3dSVM_{test}_test+tlrc \
+                -model {group_dir}/3dSVM_train+tlrc \
+                -testlabels 3dSVM_{test}_cat_updated.txt \
+                -predictions 3dSVM_pred_{test} \
+                -classout \
+                2> 3dSVM_pred_{test}_acc.txt
+        """
+        func_sbatch(h_cmd, 1, 1, 1, "MVPAtst", subj_dir)
+        # h_test = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+        # test_out = h_test.communicate()
+        # print(test, test_out)
 
 # %%
