a2d884a78dadf303ef57cc4ce5b7666e8b7ee999
Nathan Muncy <nmuncy@fiu.edu>
2021-01-14 15:31:39 -0500

    step4 done

--- mvpa_step4_comb.py
+++ mvpa_step4_comb.py
@@ -1,49 +1,57 @@
-# %%
 import os
 import fnmatch
 import pandas as pd
 import numpy as np
 
-# %%
-deriv_dir = "/scratch/madlab/nate_vCAT/derivatives"
-beh_dir = os.path.join(deriv_dir, "grpAnalysis/behAnalysis")
-test_list = ["Study_BE", "Study_BP", "Study_CP", "Study_FP"]
-sess_str = "ses-S1"
-cat_dict = {"Face": 1, "Scene": 2}
 
-if not os.path.exists(beh_dir):
-    os.makedirs(beh_dir)
+def main():
+    """
+    This script will combine category and predicted files, split for
+        category type, and write out to beh_dir for each test.
+    """
 
-# %%
+    # Set up
+    deriv_dir = "/scratch/madlab/nate_vCAT/derivatives"
+    beh_dir = os.path.join(deriv_dir, "grpAnalysis/behAnalysis")
+    test_list = ["Study_BE", "Study_CE", "Study_FP"]
+    sess_str = "ses-S1"
+    cat_dict = {"Face": 1, "Scene": 2}
 
-subj_list = [x for x in os.listdir(deriv_dir) if fnmatch.fnmatch(x, "sub-*")]
-subj_list.sort
+    # make output dir
+    if not os.path.exists(beh_dir):
+        os.makedirs(beh_dir)
 
-# for test in test_list:
-test = test_list[0]
+    # get subjs
+    subj_list = [x for x in os.listdir(deriv_dir) if fnmatch.fnmatch(x, "sub-*")]
+    subj_list.sort()
 
-# start master df
-master_df = pd.DataFrame()
+    for subj in subj_list:
+        subj_dir = os.path.join(deriv_dir, subj, sess_str)
 
-# for subj in subj_list:
-subj = subj_list[1]
-subj_dir = os.path.join(deriv_dir, subj, sess_str)
+        for test in test_list:
 
-# Combine true, predicted categories
-df_subj = pd.read_table(os.path.join(subj_dir, f"3dSVM_{test}_cat_updated.txt"))
-df_subj["Pred"] = pd.read_table(os.path.join(subj_dir, f"3dSVM_pred_{test}.1D"))
-df_subj.columns = ["True", "Pred"]
+            # Combine true, predicted categories
+            df_subj = pd.read_table(
+                os.path.join(subj_dir, f"MVPA_{test}_cat_updated.txt")
+            )
+            df_subj["Pred"] = pd.read_table(
+                os.path.join(subj_dir, f"MVPA_pred_{test}.1D")
+            )
+            df_subj.columns = ["True", "Pred"]
 
-# Split into individual cat files, for ROC
-for cat in cat_dict:
-    np.savetxt(
-        os.path.join(subj_dir, f"3dSVM_table_{test}_{cat}.txt"),
-        df_subj[df_subj["True"].astype(str).str.contains(f"{cat_dict[cat]}")],
-        fmt="%s",
-        delimiter=",",
-        header="True,Pred",
-        comments="",
-    )
+            # Split into individual cat files, for ROC
+            for cat in cat_dict:
+                np.savetxt(
+                    os.path.join(beh_dir, f"{subj}_{test}_{cat}.txt"),
+                    df_subj[
+                        df_subj["True"].astype(str).str.contains(f"{cat_dict[cat]}")
+                    ],
+                    fmt="%s",
+                    delimiter=",",
+                    header="True,Pred",
+                    comments="",
+                )
 
 
-# %%
+if __name__ == "__main__":
+    main()
