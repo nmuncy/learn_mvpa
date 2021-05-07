085ee24ad1ebfa9ad71e591489022c5e39ccbb98
Nathan Muncy <nmuncy@fiu.edu>
2021-05-06 21:42:25 -0400

    updated fmap correction

--- gp_step1_submit.py
+++ gp_step1_submit.py
@@ -10,7 +10,6 @@ phase_list = list of phases gathered within a single session.
     during the same session, then phase_list = ["study", "test"]
 """
 
-# %%
 import os
 from datetime import datetime
 import subprocess
@@ -22,7 +21,6 @@ code_dir = "/home/nmuncy/compute/learn_mvpa"
 parent_dir = "/scratch/madlab/nate_vCAT"
 sess_list = ["ses-S1"]
 phase_list = ["loc", "Study"]
-blip_toggle = 1  # 1 = on, 0 = off
 
 
 def main():
@@ -44,39 +42,37 @@ def main():
     ]
     subj_list.sort()
 
-    for i in subj_list:
-        for j in sess_list:
+    for subj in subj_list:
+        for sess in sess_list:
             if not os.path.exists(
                 os.path.join(
                     parent_dir,
                     "derivatives",
-                    i,
-                    j,
+                    subj,
+                    sess,
                     f"run-1_{phase_list[0]}_scale+tlrc.HEAD",
                 )
             ):
 
-                h_out = os.path.join(out_dir, f"out_{i}_{j}.txt")
-                h_err = os.path.join(out_dir, f"err_{i}_{j}.txt")
+                h_out = os.path.join(out_dir, f"out_{subj}_{sess}.txt")
+                h_err = os.path.join(out_dir, f"err_{subj}_{sess}.txt")
 
                 sbatch_job = f"""
                     sbatch \
-                        -J "GP1{i.split("-")[1]}" -t 10:00:00 --mem=4000 --ntasks-per-node=1 \
+                        -J "GP1{subj.split("-")[1]}" -t 10:00:00 --mem=4000 --ntasks-per-node=1 \
                         -p IB_44C_512G  -o {h_out} -e {h_err} \
                         --account iacc_madlab --qos pq_madlab \
                         --wrap="module load python-3.7.0-gcc-8.2.0-joh2xyk \n \
-                        python {code_dir}/gp_step1_preproc.py {i} {j} \
-                        {blip_toggle} {parent_dir} {' '.join(phase_list)}"
+                        python {code_dir}/gp_step1_preproc.py {subj} {sess} \
+                        {parent_dir} {' '.join(phase_list)}"
                 """
                 sbatch_submit = subprocess.Popen(
                     sbatch_job, shell=True, stdout=subprocess.PIPE
                 )
                 job_id = sbatch_submit.communicate()[0]
-                print(job_id)
+                print(job_id.decode("utf-8"))
                 time.sleep(1)
 
 
 if __name__ == "__main__":
     main()
-
-# %%
