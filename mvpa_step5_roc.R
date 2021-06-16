14e6d0dba4d29ffe3274462aa5e36aecbf8426b0
Nathan Muncy <nmuncy@fiu.edu>
2021-06-16 12:27:14 -0400

    started logistic regression

--- mvpa_step5_roc.R
+++ mvpa_step5_roc.R
@@ -1,3 +1,4 @@
+library("lme4")
 
 
 ### --- Notes
@@ -7,18 +8,68 @@
 
 
 # Set up
-data_dir <- "/Users/nmuncy/Projects/learn_mvpa/behAnalysis/"
-study_list <- c("Study_BE", "Study_CE", "Study_FP")
+data_dir <- "/Users/nmuncy/Projects/learn_mvpa/mvpa_pred/"
+study_list <- c("Study_single")
+cat_list <- c("all", "Face", "Scene")
 
 # get files, subj list
 all_list <- list.files(data_dir)
-subj_list <- vector(mode = "list", length = length(all_list)/length(study_list))
+subj_list <- vector(
+  mode = "list", 
+  length = length(all_list)/length(study_list)
+)
 for(i in 1:length(all_list)){
   subj_list[i] <- gsub("_.*","",all_list[i])
 }
 subj_list <- unique(subj_list)
 
-# make data frame
+
+# logistic regression for all & e/category
+# for(cat in ccat_list){
+#   
+# }
+cat <- cat_list[1]
+df_cat <- as.data.frame(matrix(NA, nrow=1, ncol=5))
+colnames(df_cat) <- c("Subj", "Volume", "True", "Pred", "Acc")
+
+for(subj in subj_list){
+  
+  df_subj <- read.csv(
+    paste0(data_dir, 
+     "/", 
+     subj, 
+     "_", 
+     study_list[1], 
+     "_", 
+     cat, 
+     ".txt"
+   )
+  )
+  df_subj$Subj <- rep(subj, dim(df_subj)[1])
+  df_subj$Volume <- 1:dim(df_subj)[1]
+  
+  df_subj$Acc <- NA
+  for(row in 1:dim(df_subj)[1]){
+    if(df_subj[row,]$Pred == df_subj[row,]$True){
+      df_subj[row,]$Acc <- 1
+    }else{
+      df_subj[row,]$Acc <- 0
+    }
+  }
+  
+  df_subj <- df_subj[,c(3,4,1,2,5)]
+  df_cat <- rbind(df_cat, df_subj)
+}
+df_cat <- df_cat[-1,]
+row.names(df_cat) <- 1:dim(df_cat)[1]
+
+fit <- glmer(Acc ~ Volume + (1|Subj), data=df_cat, family=binomial(link="logit"))
+
+# Unequal number of volumes, should probably do a bayesian model
+
+
+
+# do d-prime for e/subj
 for(study in study_list){
   # study <- study_list[1]
 
